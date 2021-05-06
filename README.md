2dfa3574a9eb08f9ed688a45c0a3fe37552cf645
DuhyeongKim <dduskim@gmail.com>
2021-05-06 11:33:47 +0900

    feat: handleKeyCommand 추가

--- README.md
+++ README.md
@@ -10,6 +10,8 @@
   - [x] Firebase 기본 설정
 - Editor 만들기
 
+  - [x] ~~선택한 영역만 볼드 처리 하기~~
+    - RichUtils에서 command 처리 제공
   - [ ]
 
 - Collaborative Editor로 바꾸기
--- src/components/CollaborativeEditor.js
+++ src/components/CollaborativeEditor.js
@@ -1,14 +1,28 @@
 import { useState } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 import './CollaborativeEditor.css';
 
 function CollaborativeEditor() {
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
 
+  const onChange = editorState => {
+    // const text = state.getCurrentContent().getPlainText();
+    setEditorState(editorState);
+  };
+
+  const handleKeyCommand = (command, editorState) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  };
+
   return (
     <div className="collaborative-editor">
-      <Editor editorState={editorState} onChange={setEditorState} />
+      <Editor editorState={editorState} onChange={onChange} handleKeyCommand={handleKeyCommand} />
     </div>
   );
 }
